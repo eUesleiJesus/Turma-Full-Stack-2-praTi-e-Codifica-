996c2a2a55b0c51e2cc774c3ab66cd43be22c4c0
Ueslei Tapioca <maispratiuesleijesus@gmail.com>
2024-09-04 23:38:21 -0300

    testando merge

--- lista_de_exercícios_1/006.js
+++ lista_de_exercícios_1/006.js
@@ -1,4 +1,4 @@
-
+// Ler três valores para os lados de um triângulo: A, B e C. Verificar se os lados fornecidos formam realmente um triângulo. Caso forme, deve ser indicado o tipo de triângulo: Isósceles, escaleno ou eqüilátero. Para verificar se os lados fornecidos formam triângulo: A < B + C e B < A + C e C < A + B Triângulo isósceles: possui dois lados iguais (A=B ou A=C ou B = C) Triângulo escaleno: possui todos os lados diferentes (A<>B e B <> C) Triângulo eqüilátero: possui todos os lados iguais (A=B e B=C)
 
 // const prompt = require('prompt-sync')();
 
@@ -52,7 +52,7 @@ function verificarTriangulo(ladoA, ladoB, ladoC) {
     }
   }
 
-  console.log(ladoA.toFixed(4), ladoB.toFixed(4), ladoC.toFixed(4)); // Saída: { ladoA: 5, ladoB: 7, ladoC: 9 } (valores aleatórios)
+  console.log(ladoA.toFixed(4), ladoB.toFixed(4), ladoC.toFixed(4)); 
   console.log(verificarTriangulo(ladoA, ladoB, ladoC)); 
 
   
